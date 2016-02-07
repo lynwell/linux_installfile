2daf46153fe25ba17c514197be49e1e409196144
lynwell <xwstc360@gmial.com>
2016-02-07 10:36:33 -0500

    add shadowsock and chacha20.sh file

---
+++ chacha20.sh
@@ -0,0 +1,6 @@
+sudo apt-get install build-essential
+wget https://github.com/jedisct1/libsodium/releases/download/1.0.8/libsodium-1.0.8.tar.gz
+tar xf libsodium-1.0.8.tar.gz && cd libsodium-1.0.8
+./configure && make -j2
+sudo make install
+sudo ldconfig
